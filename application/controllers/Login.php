26a9ecf3d0c9c8e9d996e3e7cb75c0c6ee0fb46a
brandondanielhr <brandondanielhr@gmail.com>
2020-06-14 16:18:32 -0300

    libreria para el token,archivo en third party:php-jwt,archivos de configuracion jwt, modificaciones en el login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+
 class Login extends CI_Controller {
 
 	function __construct()
@@ -18,12 +19,44 @@ class Login extends CI_Controller {
 	public function verificarUsuario()
 	{
 		$data = json_decode(file_get_contents("php://input"), true);
+
+		print_r($data);
 		// obtener los usuarios
 		$usuarios=$this->um->get();
+		
 		foreach ($usuarios as $usuario) {
+
+
 			if($usuario->email==$data['login'] && $usuario->contrasena==$data['passwd'])
 			{
-				return true;
+				$this->load->library("authorization_token");
+				
+				$token_data["user_id"]=$usuario->idUsuarios;
+				$token_data["nombre"]=$usuario->nombre;
+				$token_data["fecha"]=$usuario->fecha_nacimiento;
+				$token_data["email"]=$usuario->email;
+				$token_data["time"]=time();
+
+				$user_token = $this->authorization_token->generateToken($token_data);
+
+				$time=time();
+
+				$return_data=array(
+					'user_id'=>$usuario->idUsuarios,
+					'nombre'=>$usuario->nombre,
+					'email'=>$usuario->email,
+					'fecha'=>$usuario->fecha_nacimiento,
+					'time'=>$time+(60*60*24),//1 dia
+					'token'=>$user_token,
+					'result'=>[
+						'status'=>200
+					]);
+				
+				echo "<pre>"; print_r($return_data); echo "</pre>";
+
+				$this->load->view('login',$user_token);
+				
+				return $return_data;
 				/* $_SESSION['hora_sesion']=time();
 				$_SESSION['nueva_sesion']=$usuario;
 				// guardar datos en sesion de usuario
